775da88274bc9a31cf720516af1c6a0f20dba816
berdogan20 <berdogan20@ku.edu.tr>
2024-01-07 12:20:50 +0300

    Doctors list data is fetched and displayed

--- Hospital/src/app/doctors-list-item/doctors-list-item.component.ts
+++ Hospital/src/app/doctors-list-item/doctors-list-item.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Doctor} from "../doctors-list/Doctor";
 
 @Component({
   selector: 'app-doctors-list-item',
   templateUrl: './doctors-list-item.component.html',
   styleUrls: ['./doctors-list-item.component.css']
 })
-export class DoctorsListItemComponent {
+export class DoctorsListItemComponent implements OnInit{
+  @Input() doctor: Doctor;
 
+  ngOnInit(): void {
+    console.log(this.doctor); // Log doctor in ngOnInit
+    // You can perform other initialization or operations involving this.doctor here
+  }
 }
--- Hospital/src/app/doctors-list/doctors-list.component.ts
+++ Hospital/src/app/doctors-list/doctors-list.component.ts
@@ -1,25 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import {DoctorsService} from "./doctors.service";
+import {Doctor} from "./Doctor";
+
 @Component({
   selector: 'app-doctors-list',
   templateUrl: './doctors-list.component.html',
   styleUrls: ['./doctors-list.component.css']
 })
 export class DoctorsListComponent implements OnInit {
-  doctors: any[];
+  doctors: Doctor[];
 
   constructor(private doctorsService: DoctorsService) {
   }
 
   ngOnInit(): void {
-    this.getDoctors();
-  }
-
-  getDoctors(): void {
-    this.doctorsService.getDoctors()
-      .subscribe((data) => {
-        this.doctors = data;
-        console.log(this.doctors); // Do something with the data
+  this.doctorsService.getDoctors()
+    .subscribe((data: any[]) => {
+      this.doctors = data.map(doctorData => {
+        return new Doctor(
+          doctorData[0], // doctor_id
+          doctorData[1], // gender
+          doctorData[2], // lname
+          doctorData[3], // specialization
+          doctorData[4], // fname
+          doctorData[5], // department_id
+          doctorData[6], // email
+          doctorData[7]  // phone_number
+        );
       });
-  }
+      console.log(this.doctors); // Ensure Doctor instances are created
+    });
+}
+
 }
