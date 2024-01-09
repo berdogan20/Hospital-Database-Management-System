007b8bf330dd0288c444ac90f79d42438e196464
berdogan20 <berdogan20@ku.edu.tr>
2024-01-09 16:24:14 +0300

    menu implemented

--- Hospital/src/app/app.module.ts
+++ Hospital/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'; // Import HttpClientModule
-
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignInComponent } from './sign-in/sign-in.component';
@@ -16,6 +16,10 @@ import { CreateAppointmentComponent } from './create-appointment/create-appointm
 import { AddDoctorComponent } from './add-doctor/add-doctor.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import {FormsModule} from "@angular/forms";
+import { MenuComponent } from './menu/menu.component';
+import { NursesListComponent } from './nurses-list/nurses-list.component';
+import { DepartmentsListComponent } from './departments-list/departments-list.component';
+import { AppointmentsListComponent } from './appointments-list/appointments-list.component';
 
 @NgModule({
   declarations: [
@@ -30,13 +34,27 @@ import {FormsModule} from "@angular/forms";
     DoctorCommentComponent,
     CreateAppointmentComponent,
     AddDoctorComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    MenuComponent,
+    NursesListComponent,
+    DepartmentsListComponent,
+    AppointmentsListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', component: WelcomeComponent, pathMatch: 'full' },
+      { path: 'welcome', component: WelcomeComponent },
+      { path: 'menu', component: MenuComponent },
+      { path: 'doctors', component: DoctorsListComponent },
+      { path: 'patients', component: PatientListComponent},
+      { path: 'nurses', component: NursesListComponent},
+      { path: 'appointments', component: AppointmentsListComponent},
+      { path: 'departments', component: DepartmentsListComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
