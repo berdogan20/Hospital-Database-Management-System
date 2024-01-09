5c6e848c0ad87dd0b381b00995be86974cdcbbe3
berdogan20 <berdogan20@ku.edu.tr>
2024-01-09 17:47:36 +0300

    routing

---
+++ Hospital/src/app/nurses-list/nurses-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nurses-list',
+  templateUrl: './nurses-list.component.html',
+  styleUrls: ['./nurses-list.component.css']
+})
+export class NursesListComponent {
+
+}
